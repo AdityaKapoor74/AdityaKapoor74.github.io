b1e90a1143dee83d2c17887f20e1ee01851ce14e
Aditya Kapoor <adityakapoor074@gmail.com>
2021-03-28 17:59:12 +0530

    update

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ function loadSkills(skills){
 
 			var row = '<div class="row">';
 			for(j=i;j<i+6&&j<skills.length;j++){
-				var skill = '<div class="col m2"><object type="image/svg+xml" data="img/'+skills[j].icon+'" width="128" height="128"></object>'+skills[j].name+'</div>';
+				var skill = '<div class="col m2"><object type="image/svg+xml" data="img/'+skills[j].icon+'" style="width:128px;height:128px;"></object>'+skills[j].name+'</div>';
 				row+=skill;
 				
 			}
