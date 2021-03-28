10f8aa5cb4c1f641a008d6d9c91534ea9a600561
Aditya Kapoor <adityakapoor074@gmail.com>
2021-03-28 17:35:34 +0530

    Skills

--- js/index.js
+++ js/index.js
@@ -258,7 +258,7 @@ $.get("js/profile.json",
 		loadLikes(profile.likes);
 		$('#helloText').html(profile.helloText);
 		loadLinks(profile.profileLinks);
-		// loadSkills(profile.skills);
+		loadSkills(profile.skills);
 		loadProjects(profile.projects);
 		loadWorks(profile.experince);
 		loadEducations(profile.educations);
